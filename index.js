3ef1671ab873b906c171272444a965f5c9ef4235
Malovanyi <malovaniyy@gmail.com>
2024-10-26 00:15:17 +0300

    4.2

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-let userName;
+// 4.1
+// let userName;
 
-while (!userName) {
-    userName = prompt("Введіть ваше імя");
+// while (!userName) {
+//     userName = prompt("Введіть ваше імя");
+// }
+
+// alert(`Hello, ${userName}! How are you?`);
+
+//4.2
+function checkDigits(number) {
+    const numberToString = number.toString();
+    const digit1 = numberToString[0];
+    const digit2 = numberToString[1];
+    const digit3 = numberToString[2];
+
+    console.log("Всі цифри однакові:", digit1 === digit2 && digit2 === digit3);
+
+    console.log("Є однакові цифри:", (digit1 === digit2 || digit1 === digit3 || digit2 === digit3),);
 }
 
-alert(`Hello, ${userName}! How are you?`);
+const inputNum = prompt("Введіть тризначне число:");
+checkDigits(Number(inputNum));
